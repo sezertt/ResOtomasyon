a964c552b981d6dfe4e0cd9b379d9764dfb35005
Sezer Teleke <sezertt@hotmail.com>
2014-09-08 10:13:44 +0300

    toplumesaj komutu eklendi

--- ResOtomasyon/src/main/java/com/res_otomasyon/resotomasyon/GlobalApplication.java
+++ ResOtomasyon/src/main/java/com/res_otomasyon/resotomasyon/GlobalApplication.java
@@ -36,13 +36,8 @@ public class GlobalApplication extends Application {
 
     public enum Komutlar {
         Default, OdemeBilgileriTablet, siparis, iptal, hesapOdeniyor, masaGirilebilirMi, masaDegistir, urunTasindi, ikram, ikramIptal,
-        BulunanYazicilar, giris, IndirimOnay, OdemeOnay, LoadSiparis, OdenenleriGonder, departman,
-<<<<<<< HEAD
-        masaAcildi, masaKapandi, AdisyonNotu, IslemHatasi, dosyalar, guncellemeyiBaslat, aktarimTamamlandi, baglanti, modemBilgileri, bildirim, bildirimBilgileri, toplumesaj
-=======
-        masaAcildi, masaKapandi, AdisyonNotu, IslemHatasi, dosyalar, guncellemeyiBaslat, aktarimTamamlandi, baglanti,
-        modemBilgileri, bildirim,bildirimBilgileri
->>>>>>> origin/master
+        BulunanYazicilar, giris, IndirimOnay, OdemeOnay, LoadSiparis, OdenenleriGonder, departman,masaAcildi, masaKapandi, AdisyonNotu,
+        IslemHatasi, dosyalar, guncellemeyiBaslat, aktarimTamamlandi, baglanti, modemBilgileri, bildirim, bildirimBilgileri, toplumesaj
     }
 
     public void connectServer(Handler myHandler) throws Exception {
